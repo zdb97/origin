6d90cbb43020fa6a79a4be4121779a6477ecb9c4
zdb97 <bianzidan@gmail.com>
2013-05-13 00:26:03 +1000

    update plan swipe

--- plan_swipe/files/planSwipe.js
+++ plan_swipe/files/planSwipe.js
@@ -30,9 +30,7 @@ $.SwipePlans.prototype = {
         
         $.each($(this.config.$planContainer, this.config.$activePlansContainer), function () {
             if ($(this).is(':visible')) {
-
-                self.buildPlanIndex($('> ul > li', $(this)).length);
-
+ 
                 window.planSwipe = new Swipe($(this).get(0), {
                     // startSlide: 4,
                     // auto: 3000,
@@ -40,16 +38,23 @@ $.SwipePlans.prototype = {
                     // disableScroll: true,
                     // stopPropagation: true,
                     // transitionEnd: function(index, element) {}
-                    callback: function (index) {
+                    callback: function (index, element) {
                         $.each($(self.config.planIndicatorItem), function () {
-                            $(this).attr('class', '');
+                            $(this).attr('class', ''); 
                         });
 						
-						console.log(index + ' ppppp');
-                        $(self.config.planIndicatorItem).eq(index).attr('class', 'on');
+						console.log(index + ' pppps');
+                        
+						//self.buildPlanIndex($(element).siblings().addBack().length);
+						$(self.config.planIndicatorItem).eq(index).attr('class', 'on');
+						
+						$(self.config.activePlansContainer).css({ 'opacity' : 0.5 });
+						$(self.config.activePlansContainer).eq(index).css({ 'opacity' : 1 });
                     }
                 });
                 
+				//console.log('muuuu');
+                self.buildPlanIndex($('> ul > li', $(this)).length);
                 self.customizePlanSwiper(); 
             }
         });
@@ -59,12 +64,14 @@ $.SwipePlans.prototype = {
         for (var i = 0; i < len; i++) {
             if (i === 0) {
                 this.config.$planIndicatorContainer.append('<li itemIndex="' + i + '" class="on">');
+				$(this.config.activePlansContainer).eq(i).css({ 'opacity' : 1 });  
             }
             else {
                 this.config.$planIndicatorContainer.append('<li itemIndex="' + i + '">');
+				$(this.config.activePlansContainer).eq(i).css({ 'opacity' : 0.5 });
             }
         }
-        
+
         $(this.config.planIndicatorItem).on('hover', function () {
             window.planSwipe.slide($(this).attr('itemIndex'), 300);
         });
@@ -80,11 +87,10 @@ $.SwipePlans.prototype = {
         //console.log($(self.config.activePlansContainer).length);
         // apple the new calculation
 		$.each($(self.config.activePlansContainer), function (i) {
-            console.log($(this).outerWidth() + ' ===-');
+            //console.log($(this).outerWidth() + ' ===-');
             $(this).css('width', planContainerWidth + 'px');
             $(this).css('left', (i * ItemGap * (-1)) + 'px');
 			
-			console.log('hahahaaa');
 			//console.log($(this).css('transform'));
 			
         });
