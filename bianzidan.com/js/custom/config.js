91c6ffe04fc68e9cbc337ff147f591dcc1ba62a9
zdb97 <bianzidan@gmail.com>
2013-08-30 01:03:19 +1000

    add testing require.js module

---
+++ bianzidan.com/js/custom/config.js
@@ -0,0 +1,34 @@
+require.config({
+	baseUrl: "js/",
+	paths: {
+		custom: "custom/",
+		lib: "lib/",
+		jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min",
+		underscore: "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min",
+		backbone: "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min"
+		/*
+		jquery: "lib/jquery-1.10.2.min",
+		underscore: "lib/underscore-min",
+		backbone: "lib/backbone-min"
+		*/
+	},
+	/* 
+	 * shim: http://requirejs.org/docs/api.html#config-shim
+	 * only use shim config for non-AMD scripts, scripts that do not already call define(). The shim config will not work correctly if used on AMD scripts,
+	 */
+	shim: {
+		'backbone': {
+			deps: ['underscore', 'jquery'],
+			exports: 'Backbone'
+		},
+		'underscore': {
+			exports: '_'
+		}
+	}
+});
+
+// Load the main app module to start the app
+requirejs(["custom/main"]);
+
+
+// example: https://github.com/requirejs/example-jquery-cdn
---
+++ bianzidan.com/js/custom/main.js
@@ -0,0 +1,21 @@
+define(
+	[
+		"jquery", "underscore", "backbone"
+	], 
+	function($, _, Backbone) {
+		console.log("test");
+		
+		$("body").prepend("heyyy");
+		
+		
+		// underscore test
+		_.each([1,2,3], function(num, index){
+			console.log(num);
+			$("body").prepend(num + "<br/>");
+		})
+		
+		
+		
+});
+
+
